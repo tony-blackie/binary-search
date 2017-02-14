979bcfb0b7d48f7484422be8007544d7b5df7fa5
MacBook <jim.casey321@gmail.com>
2017-02-14 23:57:56 +0200

    Add logic for fibonacci method

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,5 +1,19 @@
 public class Fibonacci {
     public static void main(String args[]) {
+        for(int i = 0; i < 10; i++) {
+            System.out.println(calculateFibonacciNumber(i));
+        }
+    }
 
+    public static int calculateFibonacciNumber(int number) {
+        if (number == 0) {
+            return 0;
+        }
+
+        if (number == 1) {
+            return 1;
+        }
+
+        return calculateFibonacciNumber(number - 1) + calculateFibonacciNumber(number - 2);
     }
 }
