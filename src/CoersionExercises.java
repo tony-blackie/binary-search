e59693cbd7d46a771ffc16b4d432a6e6b460d287
MacBook <jim.casey321@gmail.com>
2017-02-15 16:11:22 +0200

    Add new array

--- src/CoersionExercises.java
+++ src/CoersionExercises.java
@@ -5,5 +5,13 @@ public class CoersionExercises {
         System.out.println(1 + 2 + 3 + 4.0);
         System.out.println(4.1 >= 4);
         System.out.println(1 + 2 + "3");
+        System.out.println("------");
+
+        int[] a = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            a[i] = i * i;
+            System.out.println(a[i]);
+        }
     }
 }
